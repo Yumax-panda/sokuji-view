68fabc39cdeedb9d608d714032a8ab29bf679831
Yumax-panda <93650251+Yumax-panda@users.noreply.github.com>
2023-04-22 15:49:31 +0900

    deleted unused part

--- src/main.js
+++ src/main.js
@@ -8,24 +8,19 @@ function getParam(name, url) {
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
-function loadAPI(rS){
+function loadAPI(){
     const user = getParam('user');
-    fetch(`https://sokujiapi.onrender.com/user/${user}`).then(r=>{return r.json()}).then(function(jsonData){
-        rS(jsonData);
-    });
+    fetch(`https://sokujiapi.onrender.com/user/${user}`)
+    .then(r=>{return r.json()})
+    .then(data => update(data));
     return 0;
 }
 
-function reflectSokuji(l){
-    const teams = l.teams;
-    const scores = l.scores;
-    left = l.left;
+function update(data){
+    const teams = data.teams;
+    const scores = data.scores;
+    left = data.left;
     teamNum = teams.length;
-    if(prev_teamNum === -1){
-        prev_teamNum = teamNum;
-    }else if(!(prev_teamNum === teamNum) && !(teamNum === -1)){
-        location.reload();
-    }
     if(prev_left === 0 && !(left === 0)){
         location.reload()
     }else if(prev_left - left > 1){
@@ -33,67 +28,52 @@ function reflectSokuji(l){
     }else{
         prev_left = left;
     }
-    document.getElementById(`team-num-${teamNum}`).style.setProperty('display', 'flex');
-    document.querySelectorAll(`#team-num-${teamNum}`).forEach($container => {
-        if (teamNum === 2) {
-            const dif = l.dif;
-            const win = l.win;
-            $container.querySelectorAll('.score').forEach(($score, i) => {
-                $score.innerText = scores[i];
-            });
-            $container.querySelectorAll('.team-span').forEach(($team, i) => {
-                $team.innerText = teams[i];
-            });
-            $container.querySelectorAll('.score-dif').forEach(($dif, i) => {
-                $dif.classList.remove('plus');
-                $dif.classList.remove('minus');
-                $dif.classList.add((dif.startsWith('+')) ? 'plus' : 'minus');
-                $dif.innerText = dif;
-            });
-            $container.querySelectorAll('.win').forEach(($win, i) => {
-                $win.style.setProperty('display', win ? 'block' : 'none');
-            });
-            $container.querySelectorAll('.left-race').forEach(($race, i) => {
-                $race.innerText = `残レース:${left}`;
-            });
-        } else {
-            $container.querySelectorAll('.score').forEach(($score, i) => {
-                $score.innerText = scores[i];
-            });
-            $container.querySelectorAll('.team-span').forEach(($team, i) => {
-                $team.innerText = teams[i];
-            });
-            $container.querySelectorAll('.dif').forEach(($dif, i) => {
-                $dif.innerText = `+${scores[i] - scores[i+1]}`;
-            });
-            $container.querySelectorAll('.left-race').forEach(($race, i) => {
-                $race.innerText = `残${left}`;
-            });
+    document.getElementById("team-num-2").style.setProperty('display', 'flex');
+    document.querySelectorAll("#team-num-2").forEach($container => {
+    const dif = data.dif;
+    const win = data.win;
+    $container.querySelectorAll('.score').forEach(($score, i) => {
+        $score.innerText = scores[i];
+    });
+    $container.querySelectorAll('.team-span').forEach(($team, i) => {
+        $team.innerText = teams[i];
+    });
+    $container.querySelectorAll('.score-dif').forEach(($dif, i) => {
+        $dif.classList.remove('plus');
+        $dif.classList.remove('minus');
+        $dif.classList.add((dif.startsWith('+')) ? 'plus' : 'minus');
+        $dif.innerText = dif;
+    });
+    $container.querySelectorAll('.win').forEach(($win, _) => {
+        $win.style.setProperty('display', win ? 'block' : 'none');
+    });
+    $container.querySelectorAll('.left-race').forEach(($race, _) => {
+        $race.innerText = `残レース:${left}`;
+    });
+    $container.querySelectorAll('.team-span').forEach($span => {
+        var $parent = $span.parentNode;
+        var scaleX = 1;
+        var translateX = 0;
+        if ($span.offsetWidth > ($parent.offsetWidth - 20)) {
+            scaleX = ($parent.offsetWidth - 20) / $span.offsetWidth;
+            translateX = 10;
         }
-        $container.querySelectorAll('.team-span').forEach($span => {
-            var $parent = $span.parentNode;
-            var scaleX = 1;
-            var translateX = 0;
-            if ($span.offsetWidth > ($parent.offsetWidth - 20)) {
-                scaleX = ($parent.offsetWidth - 20) / $span.offsetWidth;
-                translateX = 10;
-            }
-            $span.style.setProperty('transform', `translateX(${translateX}px) scaleX(${scaleX})`);
-        });
+        $span.style.setProperty('transform', `translateX(${translateX}px) scaleX(${scaleX})`);
+    });
     });
 }
 
 
-var teamNum = -1;
-var prev_teamNum = -1;
+var teamNum = 2;
+var prev_teamNum = 2;
 var left = -1;
 var prev_left = -1;
 var interval;
-loadAPI(reflectSokuji);
+loadAPI(update);
 
 
 intervalId = setInterval(function(){
-    loadAPI(reflectSokuji);
+    loadAPI(update);
 }, 5000);
 
 setTimeout(function(){
